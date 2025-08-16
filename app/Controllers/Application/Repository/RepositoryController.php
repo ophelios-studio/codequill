cbd5e8743006155cad6ab662ea8f32df69acdc28
dtucker <davidt2003@msn.com>
2025-08-16 12:06:40 -0400

    feat: draft to list repositories

---
+++ app/Controllers/Application/Repository/RepositoryController.php
@@ -0,0 +1,31 @@
+<?php namespace Controllers\Application\Repository;
+
+use Pulsar\Account\Passport;
+use Pulsar\OAuth\GitHub\GitHubService;
+use Tracy\Debugger;
+use Zephyrus\Application\Flash;
+use Zephyrus\Network\Response;
+use Zephyrus\Network\Router\Get;
+
+class RepositoryController extends CodeBaseController
+{
+    #[Get("/{org}/repositories")]
+    public function index(string $organizationLogin): Response
+    {
+        $token = Passport::getUser()->authentication->oauth_access_token;
+        $service = new GitHubService($token);
+
+        $repositories = ($organizationLogin == "me")
+            ? $service->getUserRepositories()
+            : $service->getOrganizationRepositories($organizationLogin);
+
+        if (empty($repositories)) {
+            Flash::warning("No repository found for this organization.");
+            return $this->redirect($this->getRouteRoot());
+        }
+        Debugger::barDump($repositories);
+        return $this->render("application/repository/repositories", [
+            'repositories' => $repositories
+        ]);
+    }
+}
