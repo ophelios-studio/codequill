2924173ab07246655e4554ce2f8aebe438593287
dtucker <davidt2003@msn.com>
2025-08-16 01:01:58 -0400

    feat: change password

--- app/Controllers/Application/ProfileController.php
+++ app/Controllers/Application/ProfileController.php
@@ -1,7 +1,11 @@
 <?php namespace Controllers\Application;
 
+use Pulsar\Account\Passport;
+use Pulsar\Account\Services\UserService;
+use Zephyrus\Application\Flash;
 use Zephyrus\Network\Response;
 use Zephyrus\Network\Router\Get;
+use Zephyrus\Network\Router\Post;
 use Zephyrus\Network\Router\Root;
 
 #[Root("/profile")]
@@ -16,6 +20,24 @@ class ProfileController extends AppController
     #[Get("/password")]
     public function changePasswordForm(): Response
     {
+        $user = Passport::getUser();
+        if ($user->authentication->oauth_provider) {
+            Flash::error(localize("accounts.errors.password_oauth"));
+            return $this->redirect("/app/profile");
+        }
         return $this->render("application/profile/password");
+    }
+
+    #[Post("/password")]
+    public function changePassword(): Response
+    {
+        $user = Passport::getUser();
+        if ($user->authentication->oauth_provider) {
+            Flash::error(localize("accounts.errors.password_oauth"));
+            return $this->redirect("/app/profile");
+        }
+        UserService::updatePassword(Passport::getUser(), $this->buildForm(), true);
+        Flash::success(localize("accounts.success.password_updated"));
+        return $this->redirect($this->getRouteRoot());
     }
 }
