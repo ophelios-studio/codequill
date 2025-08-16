4cfa92700f8b8a08a80df11e535fe40254281e74
dtucker <davidt2003@msn.com>
2025-08-15 23:03:53 -0400

    feat: add github signup process

--- app/Controllers/Public/SignupController.php
+++ app/Controllers/Public/SignupController.php
@@ -1,9 +1,12 @@
 <?php namespace Controllers\Public;
 
 use Controllers\Controller;
+use Pulsar\Account\Passport;
 use Pulsar\Account\Services\AuthenticationService;
 use Pulsar\Account\Services\UserService;
+use Pulsar\OAuth\GitHub\Entities\GitHubUser;
 use Zephyrus\Application\Flash;
+use Zephyrus\Core\Session;
 use Zephyrus\Network\Response;
 use Zephyrus\Network\Router\Get;
 use Zephyrus\Network\Router\Post;
@@ -17,17 +20,6 @@ class SignupController extends Controller
         return $this->render("public/signup");
     }
 
-    #[Post("/signup")]
-    public function signup(): Response
-    {
-        $user = UserService::signup($this->buildForm());
-        Flash::success(localize("accounts.success.signup", [
-            'fullname' => $user->fullname,
-            'email' => MaskFormat::email($user->email)]
-        ));
-        return $this->redirect("/login");
-    }
-
     #[Get("/signup-activation/{code}")]
     public function signupActivation(string $code): Response
     {
@@ -39,5 +31,50 @@ class SignupController extends Controller
         AuthenticationService::activate($user);
         Flash::success(localize("accounts.success.activation", ['email' => $user->email]));
         return $this->redirect("/login");
+    }
+
+    #[Post("/signup")]
+    public function signup(): Response
+    {
+        $user = UserService::signup($this->buildForm());
+        Flash::success(localize("accounts.success.signup", [
+            'fullname' => $user->fullname,
+            'email' => MaskFormat::email($user->email)]
+        ));
+        return $this->redirect("/login");
+    }
+
+    #[Get("/signup-github")]
+    public function signupGitHubForm(): Response
+    {
+        $provider = Session::get('oauth_provider');
+        if ($provider != 'github') {
+            Flash::error(localize("accounts.errors.invalid_provider"));
+            return $this->redirect("/login");
+        }
+        $user = GitHubUser::build(Session::get('oauth_user'));
+        return $this->render("public/signup-github", [
+            'user' => $user
+        ]);
+    }
+
+    #[Post("/signup-github")]
+    public function signupGitHub(): Response
+    {
+        $provider = Session::get('oauth_provider');
+        if ($provider != 'github') {
+            Flash::error(localize("accounts.errors.invalid_provider"));
+            return $this->redirect("/login");
+        }
+        $gitHubUser = GitHubUser::build(Session::get('oauth_user'));
+        $token = Session::get('oauth_access_token');
+        $user = UserService::signupByGitHub($this->buildForm(), $gitHubUser->getRawData(), $token);
+        Flash::success(localize("accounts.success.signup_github", ['fullname' => $user->fullname]));
+        UserService::updateLastConnection($user->id);
+        Passport::registerUser($user);
+        Session::remove('oauth_provider');
+        Session::remove('oauth_user');
+        Session::remove('oauth_access_token');
+        return $this->redirect("/app/profile");
     }
 }
