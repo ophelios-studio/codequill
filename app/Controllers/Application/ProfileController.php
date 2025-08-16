a11c6bcb53ff2622977c62b880fcda69fffe68da
dtucker <davidt2003@msn.com>
2025-08-15 22:27:56 -0400

    feat: redirect to profile upon login

--- app/Controllers/Application/ProfileController.php
+++ app/Controllers/Application/ProfileController.php
@@ -1,11 +1,13 @@
 <?php namespace Controllers\Application;
 
 use Zephyrus\Network\Response;
+use Zephyrus\Network\Router\Get;
 use Zephyrus\Network\Router\Root;
 
 #[Root("/profile")]
 class ProfileController extends AppController
 {
+    #[Get("/")]
     public function index(): Response
     {
         return $this->html("<h3>You have successfully logged in! More to come.</h3>");
