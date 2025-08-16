7a59f0f4514e37e34e875248e28e1d53ac28de68
dtucker <davidt2003@msn.com>
2025-08-16 10:05:42 -0400

    feat: get token prices with caching

--- app/Controllers/Application/AppController.php
+++ app/Controllers/Application/AppController.php
@@ -1,8 +1,11 @@
 <?php namespace Controllers\Application;
 
 use Controllers\Controller;
+use Models\Account\Services\PriceService;
+use Models\Account\Services\TokenService;
 use Models\Account\Services\WalletService;
 use Pulsar\Account\Passport;
+use Tracy\Debugger;
 use Zephyrus\Network\Response;
 use Zephyrus\Network\Router\Authorize;
 
@@ -13,8 +16,20 @@ abstract class AppController extends Controller
     {
         $args = array_merge($args, [
             'user' => Passport::getUser(),
-            'wallet' => new WalletService()->getConnectedWallet(Passport::getUserId())
+            'wallet' => new WalletService()->getConnectedWallet(Passport::getUserId()),
+            'prices' => $this->getPrices()
         ]);
         return parent::render($page, $args);
+    }
+
+    private function getPrices(): array
+    {
+        $priceService = new TokenService();
+        $eth = $priceService->getEth();
+        $booe = $priceService->getBooe();
+        return [
+            'eth' => $eth->usdPrice,
+            'booe' => $booe->usdPrice,
+        ];
     }
 }
