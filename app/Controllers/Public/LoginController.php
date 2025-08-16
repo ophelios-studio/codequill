e4eaf630ed51c63e39a137cd045e76c2ddc60cc2
dtucker <davidt2003@msn.com>
2025-08-15 22:10:32 -0400

    feat: add basic authentication

--- app/Controllers/Public/LoginController.php
+++ app/Controllers/Public/LoginController.php
@@ -1,8 +1,12 @@
 <?php namespace Controllers\Public;
 
 use Controllers\Controller;
+use Pulsar\Account\Authenticator;
+use Pulsar\Account\Exceptions\AuthenticationException;
+use Zephyrus\Application\Flash;
 use Zephyrus\Network\Response;
 use Zephyrus\Network\Router\Get;
+use Zephyrus\Network\Router\Post;
 
 class LoginController extends Controller
 {
@@ -10,5 +14,17 @@ class LoginController extends Controller
     public function index(): Response
     {
         return $this->render("public/login");
+    }
+
+    #[Post("/")]
+    public function login(): Response
+    {
+        try {
+            new Authenticator()->login();
+        } catch (AuthenticationException $e) {
+            Flash::error($e->getUserMessage());
+            return $this->redirect("/login");
+        }
+        return $this->redirect("/app");
     }
 }
