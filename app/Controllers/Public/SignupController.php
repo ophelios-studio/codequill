268222cab89ae2d65b7266e48ce53dde570a8e8c
dtucker <davidt2003@msn.com>
2025-08-15 22:25:48 -0400

    feat: add basic signup

--- app/Controllers/Public/SignupController.php
+++ app/Controllers/Public/SignupController.php
@@ -1,8 +1,12 @@
 <?php namespace Controllers\Public;
 
 use Controllers\Controller;
+use Pulsar\Account\Services\UserService;
+use Zephyrus\Application\Flash;
 use Zephyrus\Network\Response;
 use Zephyrus\Network\Router\Get;
+use Zephyrus\Network\Router\Post;
+use Zephyrus\Utilities\MaskFormat;
 
 class SignupController extends Controller
 {
@@ -10,5 +14,16 @@ class SignupController extends Controller
     public function index(): Response
     {
         return $this->render("public/signup");
+    }
+
+    #[Post("/signup")]
+    public function signup(): Response
+    {
+        $user = UserService::signup($this->buildForm());
+        Flash::success(localize("accounts.success.signup", [
+            'fullname' => $user->fullname,
+            'email' => MaskFormat::email($user->email)]
+        ));
+        return $this->redirect("/login");
     }
 }
