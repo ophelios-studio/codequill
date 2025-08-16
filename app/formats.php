0a90fcb29c5474efd3f0b4ac2d5d5c1c204b723e
dtucker <davidt2003@msn.com>
2025-08-16 08:32:17 -0400

    feat: add custom eth address format

--- app/formats.php
+++ app/formats.php
@@ -13,3 +13,18 @@ Formatter::register('day_full', function ($dateTime) {
     $formatter = new IntlDateFormatter(Locale::getDefault(), IntlDateFormatter::LONG, IntlDateFormatter::SHORT, null, null, "EEEE");
     return $formatter->format($dateTime->getTimestamp());
 });
+
+Formatter::register('eth_address', function (string $address) {
+    if (empty($address)) {
+        return '';
+    }
+
+    // Ensure address is properly formatted
+    $address = strtolower($address);
+    if (str_starts_with($address, '0x')) {
+        $address = substr($address, 2);
+    }
+
+    return '0x' . substr($address, 0, 6) . '...' . substr($address, -4);
+});
+
