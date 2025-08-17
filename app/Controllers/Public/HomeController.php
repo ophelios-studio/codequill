e1ddd44e5983443843a5a80add537be5647e7a60
dtucker <davidt2003@msn.com>
2025-08-17 05:47:40 -0400

    feat: homepage first version

--- app/Controllers/Public/HomeController.php
+++ app/Controllers/Public/HomeController.php
@@ -1,6 +1,7 @@
 <?php namespace Controllers\Public;
 
 use Controllers\Controller;
+use Models\Account\Services\TokenService;
 use Zephyrus\Network\Response;
 use Zephyrus\Network\Router\Get;
 
@@ -9,12 +10,25 @@ class HomeController extends Controller
     #[Get("/")]
     public function index(): Response
     {
-        return $this->render("welcome");
+        return $this->render("welcome", [
+            'prices' => $this->getPrices(),
+        ]);
     }
 
     #[Get("/health")]
     public function health(): Response
     {
         return $this->json(['status' => 'ok']);
+    }
+
+    private function getPrices(): array
+    {
+        $priceService = new TokenService();
+        $eth = $priceService->getEth();
+        $booe = $priceService->getBooe();
+        return [
+            'eth' => $eth->usdPrice,
+            'booe' => $booe->usdPrice,
+        ];
     }
 }
