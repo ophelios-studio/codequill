b191793c4144484b9190a654bef27f789f607236
dtucker <davidt2003@msn.com>
2025-08-17 03:54:37 -0400

    feat: protect contributed projects

--- app/Controllers/Application/AppController.php
+++ app/Controllers/Application/AppController.php
@@ -1,11 +1,10 @@
 <?php namespace Controllers\Application;
 
 use Controllers\Controller;
-use Models\Account\Services\PriceService;
 use Models\Account\Services\TokenService;
 use Models\Account\Services\WalletService;
 use Pulsar\Account\Passport;
-use Tracy\Debugger;
+use Pulsar\OAuth\GitHub\GitHubService;
 use Zephyrus\Network\Response;
 use Zephyrus\Network\Router\Authorize;
 
@@ -14,10 +13,15 @@ abstract class AppController extends Controller
 {
     public function render(string $page, array $args = []): Response
     {
+        $user = null;
+        if (Passport::getUser()->authentication->oauth_provider) {
+            $user = new GitHubService(Passport::getUser()->authentication->oauth_access_token)->getUser();
+        }
         $args = array_merge($args, [
             'user' => Passport::getUser(),
             'wallet' => new WalletService()->getConnectedWallet(Passport::getUserId()),
-            'prices' => $this->getPrices()
+            'prices' => $this->getPrices(),
+            'github_user' => $user
         ]);
         return parent::render($page, $args);
     }
